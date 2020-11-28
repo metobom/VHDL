8711038e195cfdcc60480b2edb4387094252e38c
metobom <46991761+metobom@users.noreply.github.com>
2020-11-28 17:46:14 +0300

    Create vhdl_template.vhd

---
+++ vhdl_template.vhd
@@ -0,0 +1,20 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity deneme is
+port (
+      -- inputs
+      a : in std_logic;
+      b : in std_logic; 
+      -- outputs
+      z : out std_logic
+      );
+end deneme;
+
+architecture Behavioral of deneme is
+    -- helper vars (wires and stuff as signal)
+    
+begin
+    -- algorithm
+
+end Behavioral;
